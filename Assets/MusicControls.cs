c16db3cd49fd62e8d6485df3a18fadb3a6cf9141
Johan <56724011+risujas@users.noreply.github.com>
2024-06-08 18:13:32 +0300

    Fully implement music controls

---
+++ Assets/MusicControls.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicControls : MonoBehaviour
+{
+	[SerializeField] private AudioSource audioSource;
+	[SerializeField] private Sprite activeSprite;
+	[SerializeField] private Sprite mutedSprite;
+
+	[SerializeField] private Image image;
+	[SerializeField] private Button button;
+	[SerializeField] private Scrollbar scrollbar;
+
+	private bool isMuted = false;
+
+	public void ToggleMute()
+	{
+		isMuted = !isMuted;
+
+		if (isMuted)
+		{
+			audioSource.volume = 0.0f;
+		}
+		else
+		{
+			audioSource.volume = scrollbar.value;
+		}
+	}
+
+	public void Unmute()
+	{
+		isMuted = false;
+		audioSource.volume = scrollbar.value;
+	}
+
+	private void HandleImageSprite()
+	{
+		if (image.sprite == activeSprite)
+		{
+			if (isMuted || audioSource.volume == 0.0f)
+			{
+				image.sprite = mutedSprite;
+			}
+		}
+		else if (image.sprite == mutedSprite)
+		{
+			if (!isMuted && audioSource.volume > 0.0f)
+			{
+				image.sprite = activeSprite;
+			}
+		}
+	}
+
+	private void Start()
+	{
+		scrollbar.value = audioSource.volume;
+	}
+
+	private void Update()
+	{
+		HandleImageSprite();
+	}
+}
